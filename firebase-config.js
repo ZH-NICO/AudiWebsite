4dcf9788295561b22646d6af582d05479cd19e16
ny-nico-s <nico.schult@noseryoung.ch>
2026-04-28 10:59:15 +0200

    Add project configuration
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "DEINE_API_KEY_HIER_EINFUEGEN",
+  authDomain: "DEIN_PROJEKT.firebaseapp.com",
+  projectId: "DEIN_PROJEKT_ID",
+  appId: "DEINE_APP_ID_HIER_EINFUEGEN"
+};
