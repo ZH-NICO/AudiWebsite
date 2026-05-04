771ba42b8141d3df27c2568e2e67670ee5605445
ny-nico-s <nico.schult@noseryoung.ch>
2026-05-04 10:51:34 +0200

    feat: WebP logo, Firebase config update, contact page with team & FAQ
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "DEINE_API_KEY_HIER_EINFUEGEN",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT_ID",
-  appId: "DEINE_APP_ID_HIER_EINFUEGEN"
+  apiKey: "AIzaSyASEf-Qgi547w5ZzfhwMvbQWx4XAHhaZmM",
+  authDomain: "audi-webshop.firebaseapp.com",
+  projectId: "audi-webshop",
+  storageBucket: "audi-webshop.firebasestorage.app",
+  messagingSenderId: "998010095520",
+  appId: "1:998010095520:web:364442083e1b6d4a9e06c5"
 };
